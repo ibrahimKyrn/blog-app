5f87aeae502ab6d8e6544fdeb4e3cf504384e8a5
Kayran <ibrahimmkyrn@gmail.com>
2023-09-14 15:27:07 +0300

    Copmponentler eklendi, css ler duzenlendi

---
+++ components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./sytyles.module.css";
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h4> copyright 2023</h4>
+    </footer>
+  );
+}
+export default Footer;
