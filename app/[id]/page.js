e0c047449ab73bd4b153ea9a3dcf9b16c2c432a0
Kayran <ibrahimmkyrn@gmail.com>
2023-09-14 20:36:13 +0300

    Bloglarin detay sayfalari olusturuldu

---
+++ app/[id]/page.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./styles.module.css";
+import Image from "next/image";
+
+async function getPost(id) {
+  const response = await fetch(`https://dummyjson.com/posts/${id}`);
+  return response.json();
+}
+
+export default async function Page({ params }) {
+  const { id, title, body } = await getPost(params.id);
+
+  return (
+    <div className={styles.blogContainer}>
+      <div className={styles.cardImage}>
+        <Image
+          src={`https://picsum.photos/960/400?random=${id}`}
+          alt={title}
+          fill
+        />
+      </div>
+      <h1 className={styles.title}>{title}</h1>
+      <p>{body}</p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import BlogCard from "../components/BlogCard";
 import styles from "./styles.module.css";
+
 async function getPosts() {
   const response = await fetch("https://dummyjson.com/posts?limit=10");
   return response.json();
@@ -9,7 +10,7 @@ export default async function Home() {
   const { posts } = await getPosts();
 
   return (
-    <div className={styles.blogContainer}>
+    <div className={styles.blogsContainer}>
       {posts.map((post) => (
         <BlogCard key={post.id} {...post} />
       ))}
