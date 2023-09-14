47eed79007d88285e9c7c076f250217eb20a91d9
Kayran <ibrahimmkyrn@gmail.com>
2023-09-14 21:58:04 +0300

    Footer degisiklik yapildi

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,7 +3,7 @@ import styles from "./sytyles.module.css";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <h4> copyright 2023</h4>
+      <h4>@2023 Ibrahim Kayran</h4>
     </footer>
   );
 }
